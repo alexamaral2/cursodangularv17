f8a1f00a2742dd7d5be594bc5c4a24a849ae9763
Alex Junior <alexjunior2018@gmail.com>
2025-09-26 16:42:18 -0300

    adicionando aulas javascript

---
+++ curso-javascript/003 - Operadores aritméticos/001 - Adição/index.js
@@ -0,0 +1,11 @@
+console.log(1 + 1);
+console.log(25 + 50);
+
+console.log("Alex " + "Junior");
+console.log("Nome:" + "Alex Junior" + ", " + "idade: " + 26 + 1);
+console.log("Nome:" + "Alex Junior" + ", " + "idade: " + (26 + 1));
+
+/*
+  + Soma
+  + Concatena
+*/
